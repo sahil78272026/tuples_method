89fe1396c2402f65493ee4dcf1f3ede76aadf2cb
sahil78272026 <sahil.garg78272026@gmail.com>
2022-04-21 03:45:32 +0530

    Add files via upload

---
+++ 05_tuple_methods.py
@@ -0,0 +1,5 @@
+# Creating a tuple using ()
+t = (1, 2, 4, 5, 4, 1, 2,1 ,1)
+
+print(t.count(1)) # return number of occurence of the value 
+print(t.index(5)) # return index  of occurence of the value
